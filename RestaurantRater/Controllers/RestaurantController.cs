b597f58e9d5e89f96e6e0af31cc7e413c46903d4
Peter-Nikolov1 <nikolov_peter@yahoo.com>
2022-01-08 21:57:30 -0500

    Added details view

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -86,7 +86,7 @@ namespace RestaurantRater.Controllers
             return View(restaurant);
         }
 
-        //POST: Restaurant/Edit/{id}
+        // POST: Restaurant/Edit/{id}
         [HttpPost]
         [ValidateAntiForgeryToken]
 
@@ -97,6 +97,21 @@ namespace RestaurantRater.Controllers
                 _db.Entry(restaurant).State = EntityState.Modified;
                 _db.SaveChanges();
                 return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+
+        // GET: Restaurant/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
             }
             return View(restaurant);
         }
